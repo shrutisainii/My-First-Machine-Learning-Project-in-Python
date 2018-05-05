f6c9c8e51a9dfbc4375273776d889769c70aba9e
Shruti Saini <shruti.saini95@gmail.com>
2018-05-06 01:36:57 +0530

    Update firstMLproject.py

--- firstMLproject.py
+++ firstMLproject.py
@@ -1,9 +1,3 @@
-
-# coding: utf-8
-
-# In[3]:
-
-
 # Load libraries
 import pandas
 from pandas.plotting import scatter_matrix
@@ -18,72 +12,27 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
-
-
-# In[4]:
-
-
 # Load dataset
 url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 names = ['sepal-length', 'sepal-width', 'petal-length', 'petal-width', 'class']
 dataset = pandas.read_csv(url, names=names)
-
-
-# In[5]:
-
-
 #shape
 print(dataset.shape)
-
-
-# In[6]:
-
-
 #head
 print(dataset.head(20))
-
-
-# In[7]:
-
-
 #descriptions
 print(dataset.describe())
-
-
-# In[8]:
-
-
 #class distribution
 print(dataset.groupby('class').size())
-
-
-# In[11]:
-
-
 #box and whisker plots
 dataset.plot(kind='box',subplots=True,layout=(2,2),sharex=False,sharey=False)
 plt.show()
-
-
-# In[12]:
-
-
 #histograms
 dataset.hist()
 plt.show()
-
-
-# In[13]:
-
-
 # scatter plot matrix
 scatter_matrix(dataset)
 plt.show()
-
-
-# In[14]:
-
-
 #Split-out validation dataset
 array = dataset.values
 X = array[ : ,0:4]
@@ -91,19 +40,9 @@ Y = array[:,4]
 validation_size=0.20
 seed=7
 X_train, X_validation , Y_train , Y_validation= model_selection.train_test_split(X,Y, test_size=validation_size,random_state=seed)
-
-
-# In[15]:
-
-
 # Test options and evaluation metric
 seed=7
 scoring='accuracy'
-
-
-# In[17]:
-
-
 # Spot check algorithms
 models=[]
 models.append(('LR',LogisticRegression()))
@@ -122,11 +61,6 @@ for name , model in models:
     names.append(name)
     msg= "%s : %f (%f)" %(name, cv_results.mean() , cv_results.std())
     print(msg)
-
-
-# In[19]:
-
-
 # compare algorithms
 fig = plt.figure()
 fig.suptitle('ALGORITHM COMPARISON')
@@ -134,11 +68,6 @@ ax=fig.add_subplot(111)
 plt.boxplot(results)
 ax.set_xticklabels(names)
 plt.show()
-
-
-# In[20]:
-
-
 #Make predictions on validation dataset
 svm=SVC()
 svm.fit(X_train,Y_train)
@@ -146,38 +75,3 @@ predictions=svm.predict(X_validation)
 print(accuracy_score(Y_validation,predictions))
 print(confusion_matrix(Y_validation,predictions))
 print(classification_report(Y_validation,predictions))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
